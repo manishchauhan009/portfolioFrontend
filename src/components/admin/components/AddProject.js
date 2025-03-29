15b46d34cb82f97a16ed1bf95e1b1d23b13caf09
Manish Chauhan <manishchauhan8401@gmail.com>
2025-03-29 17:15:45 +0530

    reslove error

--- src/components/admin/components/AddProject.js
+++ src/components/admin/components/AddProject.js
@@ -12,12 +12,11 @@ const AddProject = () => {
   const uploadImage = async (file) => {
     const formData = new FormData();
     formData.append("file", file);
-    formData.append("upload_preset", "portfolio"); // ✅ Correct key
-
+    formData.append("upload_preset", process.env.REACT_APP_CLOUDINARY_UPLOAD_PRESET);
 
     try {
       const response = await axios.post(
-        `https://api.cloudinary.com/v1_1/dninyy2on/image/upload`,
+        `https://api.cloudinary.com/v1_1/${process.env.REACT_APP_CLOUDINARY_CLOUD_NAME}/image/upload`,
         formData
       );
       return response.data.secure_url;
